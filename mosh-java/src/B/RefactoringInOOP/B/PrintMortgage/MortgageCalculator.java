c86c029ebf36a232c881bff56d646d7268274572
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-23 10:58:38 -0700

    moved getRemainingBalances method to MortCalculator

--- mosh-java/src/B/RefactoringInOOP/B/PrintMortgage/MortgageCalculator.java
+++ mosh-java/src/B/RefactoringInOOP/B/PrintMortgage/MortgageCalculator.java
@@ -1,8 +1,10 @@
 package B.RefactoringInOOP.B.PrintMortgage;
 
+import java.text.NumberFormat;
+
 public class MortgageCalculator {
-    public final static byte MONTHS_IN_YEAR = 12;
-    public final static byte PERCENT = 100;
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
 
     private int principal;
     private float annualInterest;
@@ -38,13 +40,20 @@ public class MortgageCalculator {
         return mortgage;
     }
 
+    public double[] getRemainingBalances(){
+        var balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= balances.length; month++) {
+            double balance = calculateBalance();
+            balances[month - 1] = balance;
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+        return balances;
+    }
+
     private int getNumberOfPayments() {
         return years * MONTHS_IN_YEAR;
     }
     private float getMonthlyInterest() {
         return annualInterest / PERCENT / MONTHS_IN_YEAR;
-    }
-    public short getYears() {
-        return years;
     }
 }
