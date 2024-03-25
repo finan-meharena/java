6aeaf32aa9e9c027124dc199ba01ac00297acab8
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-25 13:38:10 -0700

    add currency instace as a field

--- mosh-java/src/B/RefactoringInOOP/B/PrintMortgage/MortgageReport.java
+++ mosh-java/src/B/RefactoringInOOP/B/PrintMortgage/MortgageReport.java
@@ -4,15 +4,17 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currencyInstance;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator; // add a calculator object property/field
+        currencyInstance = NumberFormat.getCurrencyInstance();
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currencyInstance.format(mortgage);
         printHeader("Mortgage");
         System.out.println("Monthly Payments: " + mortgageFormatted);
     }
@@ -20,7 +22,7 @@ public class MortgageReport {
     public void printPaymentSchedule() {
         printHeader("PAYMENT SCHEDULE");
         for (double balance : calculator.getRemainingBalances()) {
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currencyInstance.format(balance));
         }
     }
 
