94f51bb6d0cc51175648c0ecd1cbf991a1df3c39
Finan Meharena <finanmeharenakidane@gmail.com>
2024-05-02 11:02:19 -0700

    done with generics

---
+++ advanced-java/src/B/generics/C/GenericMethods/Instructor.java
@@ -0,0 +1,8 @@
+package B.generics.C.GenericMethods;
+
+public class Instructor extends User{
+
+    public Instructor(int points){
+        super(points);
+    }
+}
--- advanced-java/src/B/generics/C/GenericMethods/Main.java
+++ advanced-java/src/B/generics/C/GenericMethods/Main.java
@@ -3,6 +3,14 @@ package B.generics.C.GenericMethods;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Utils.max(11,2));
+
        Utils.print("finan", "meharena");
+
+       User user = new User(10);
+       var users = new GenericList<User>();
+
+       Utils.printUser(user);
+
+
     }
 }
