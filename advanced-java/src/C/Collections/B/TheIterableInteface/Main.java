3aa20897122f76a960800995d8b6e8c375dca404
Finan Meharena <finanmeharenakidane@gmail.com>
2024-05-18 20:08:46 -0700

    overridden iterator method

---
+++ advanced-java/src/C/Collections/B/TheIterableInteface/Main.java
@@ -0,0 +1,9 @@
+package C.Collections.B.TheIterableInteface;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new GenericList<Integer>();
+
+//        for (var item: list.items) we will have to expose our internal implementation details
+    }
+}
